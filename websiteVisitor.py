d2c92c7cff221e91d992aa6403c746b16f56613c
Jenna C. Williams <36139094+jcwill415@users.noreply.github.com>
2020-05-28 12:56:26 -0400

    Add files via upload

---
+++ websiteVisitor.py
@@ -0,0 +1,25 @@
+# Website Vistor Automation
+# This script will open up a website, keep it for 30 seconds
+# Then close that tab and open the next
+# After it loops through all the urls listed the process ends
+
+import webbrowser
+import sys
+import time
+from time import sleep
+import os
+import keyboard
+
+##count = 0
+urls = ['https://www.google.com/', 'https://twitter.com/jcwill415', 'https://youtube.com', 'https://www.coursera.org/learn/trading-algorithm/home/welcome', 'https://www.coursera.org/learn/python-statistics-financial-analysis/home/welcome', 'https://www.coursera.org/learn/r-programming/home/welcome', 'https://tdameritrade.com', 'https://www.linkedin.com/in/jenna-williams-04a8b7146/',
+'https://www.facebook.com/profile.php?id=100013341725611', 'https://www.coursera.org/learn/dataviz-visual-analytics/home/welcome', 'https://www.google.com/docs/about/']
+
+for url in urls:
+  webbrowser.open(url, new =0)
+  time.sleep(5)
+#  keyboard.press_and_release(['Ctrl', 'W'])
+##  os.system("killall 'Google Chrome'")
+##  count = count + 0
+
+##else:
+##  pass      
